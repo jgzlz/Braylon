e092f977d32fb9bfe510b56a1895292c8efa9cc4
Joe Gonzalez <s.joe.gonzalez@gmail.com>
2020-02-12 14:39:26 -0600

    Added app class

--- Braylon/src/test/java/com/dev10/braylon/data/CustomerDaoTest.java
+++ Braylon/src/test/java/com/dev10/braylon/data/CustomerDaoTest.java
@@ -5,39 +5,25 @@
  */
 package com.dev10.braylon.data;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  *
  * @author Joe Gonzalez
  */
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class CustomerDaoTest {
+    
+    @Autowired
+    private CustomerDao customerDao;
     
     public CustomerDaoTest() {
     }
     
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
 
-    @Test
-    public void testSomeMethod() {
-    }
     
 }
