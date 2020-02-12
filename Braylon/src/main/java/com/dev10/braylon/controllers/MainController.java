a05d4d57c9a9682f38da1f38d71712be8b5c8a2d
jnwhite <whitenjacob@gmail.com>
2020-02-12 13:28:41 -0600

    customerDao

--- Braylon/src/main/java/com/dev10/braylon/controllers/MainController.java
+++ Braylon/src/main/java/com/dev10/braylon/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.dev10.braylon.controllers;
 
 
+import com.dev10.braylon.models.Order;
 import com.dev10.braylon.models.Role;
 import com.dev10.braylon.models.SalesVisit;
 import com.dev10.braylon.models.User;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public abstract class MainController {
@@ -48,9 +50,31 @@ public abstract class MainController {
             return "homeAdmin";
         }
         else {
-            List<SalesVisit> visits = uServ.findSalesByUser(currentUser.getUsername());
+            List<SalesVisit> visits = uServ.findAllSalesVisitsByUsername(currentUser.getUsername());
             model.addAttribute("orders", currentUser.getOrders());
+            model.addAttribute("salesVisits", visits);
+            
+            return "homeSalesRep";
         }
     }
     
+    //Adding SalesVisit
+    @GetMapping("/addSalesVisit")
+    public String viewAddSalesVisit(Model model, SalesVisit visit) {
+        
+        return "addSalesVisit";
+    }
+    
+    @PostMapping("/addSalesVisit")
+    public String processAddSalesVisit(Model model, SalesVisit visit) {
+        
+        return "redirect:/home";
+    }
+    
+    //Adding Order
+    @GetMapping("/addOrder")
+    public String viewAddOrder(Model model, Order order) {
+        
+        return "addOrder";
+    }
 }
