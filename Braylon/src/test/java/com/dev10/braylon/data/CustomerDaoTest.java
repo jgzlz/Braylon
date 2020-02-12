812ba8a851a49cb7e21585cf1e17e212f4022502
Joe Gonzalez <s.joe.gonzalez@gmail.com>
2020-02-12 14:00:25 -0600

    Made starter code for DAO tests.

---
+++ Braylon/src/test/java/com/dev10/braylon/data/CustomerDaoTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dev10.braylon.data;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Joe Gonzalez
+ */
+public class CustomerDaoTest {
+    
+    public CustomerDaoTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
