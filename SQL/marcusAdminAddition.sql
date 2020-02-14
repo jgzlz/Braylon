8c104b1179b2948d9a48d9052897dc3add8b207e
Marcus Elliott <m.d.elliott@outlook.com>
2020-02-14 13:37:49 -0600

    right pushed the hello, username on navbar

--- SQL/marcusAdminAddition.sql
+++ SQL/marcusAdminAddition.sql
@@ -4,3 +4,10 @@ insert into `user`values
 insert into customer values 
 (3, 'Beth', 'Raymond', '123 Main St','123-123-1234','Genesis10','beth@beth.com', 5),
 (4, 'Joe', 'Gonzalez', '10 Joe Lane','234-123-1245','Joe Gonzalez','Joe@joe.joe', 5);
+insert into sales_visit values 
+(101,'2020-02-12',2,5,'Home','ahhh'),
+(102,'2020-02-14',1,5,'Company','ahhh');
+
+insert into `bill` values 
+(101,1,'2020-02-12','2020-03-12','pending','20123.12'),
+(104,2,'2020-02-12','2020-03-12','pending','2123.12');
