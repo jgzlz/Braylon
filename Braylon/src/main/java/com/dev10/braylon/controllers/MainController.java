1bc98cb3a040e8939224613ddcfc3251bca0e505
Marcus Elliott <m.d.elliott@outlook.com>
2020-02-14 11:50:48 -0600

    made various changes to main controller, including adding a redirect to /home from /, changing /customer/customerId to /editcustomer/customerId. Also added add and edit header values to the model for add and edit customer get mappings that allow specification in the customerDetail file. Also added endpoints for the about section and the salesRepProfile section.

--- Braylon/src/main/java/com/dev10/braylon/controllers/MainController.java
+++ Braylon/src/main/java/com/dev10/braylon/controllers/MainController.java
@@ -1,6 +1,5 @@
 package com.dev10.braylon.controllers;
 
-
 import com.dev10.braylon.models.Bill;
 import com.dev10.braylon.models.Customer;
 import com.dev10.braylon.models.Product;
@@ -25,35 +24,34 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public abstract class MainController {
-    
+
     public static User currentUser;
-    
+
     @Autowired
     private customerService cServ;
-    
+
     @Autowired
     private billService bServ;
-    
+
     @Autowired
     private productService pServ;
-    
+
     @Autowired
     private salesVisitService svServ;
-    
+
     @Autowired
     private userService uServ;
-    
+
     @Autowired
     private BCryptPasswordEncoder encoder;
 
     @GetMapping("/home")
     public String loadHomePage(Model model) {
-        if(userIsAdmin()) {
+        if (userIsAdmin()) {
             model.addAttribute("orders", new ArrayList());
             model.addAttribute("salesVisits", new ArrayList());
             return "home";
-        }
-        else {
+        } else {
             List<SalesVisit> visits = uServ.findAllSalesVisitsByUsername(currentUser.getUsername());
             List<Bill> bills = bServ.findAllBillsByUsername(currentUser.getUsername());
             model.addAttribute("orders", bills);
@@ -61,85 +59,87 @@ public abstract class MainController {
             return "home";
         }
     }
-    
-    @GetMapping("/customers")
+
+    @GetMapping("/customer")
     public String loadCustomerView(Model model) {
         List<Customer> custList;
-        if(userIsAdmin()) {
+        if (userIsAdmin()) {
             custList = cServ.findAllCustomers();
-        }
-        else {
+        } else {
             custList = cServ.findAllCustomersByUsername(currentUser.getUsername());
         }
         model.addAttribute("customers", custList);
         return "customer";
     }
-    
+
     //Adding Customer
     @GetMapping("/addCustomer/{username}")
     public String loadAddingCustomer(@PathVariable String username, Model model) {
-        if(userIsAdmin()) {
+        if (userIsAdmin()) {
             model.addAttribute("salesReps", uServ.findAllSalesReps());
         }
         Customer customer = new Customer();
         model.addAttribute("customer", customer);
+        model.addAttribute("customer", customer);
+        model.addAttribute("header", "add");
         return "customerDetail";
     }
-    
+
     @PostMapping("/addCustomer/{username}")
     public String addNewCustomer(@PathVariable String username, Customer customer) {
         cServ.addCustomer(customer);
         return "redirect:/home";
     }
-    
+
     //Editing Customer
-    @GetMapping("/customer/{customerId}")
+    @GetMapping("/editcustomer/{customerId}")
     public String loadEditCustomer(Model model, @PathVariable int customerId) {
         Customer customer = cServ.findCustomerById(customerId);
-        if(userIsAdmin()) {
-            model.addAttribute("salesReps", uServ.findAllSalesReps());
+        if (userIsAdmin()) {
+            model.addAttribute("users", uServ.findAllSalesReps());
         }
         model.addAttribute("customer", customer);
+        model.addAttribute("header", "edit");
         return "customerDetail";
     }
-    
-    @PostMapping("/customer/{customerId}")
+
+    @PostMapping("/editcustomer/{customerId}")
     public String editCustomer(@PathVariable int customerId, Customer customer) {
         cServ.editCustomer(customer);
         return "redirect:/home";
     }
-    
+
     //Adding SalesRep
     @GetMapping("/addSalesRep")
     public String loadAddingSalesRep(Model model) {
         User user = new User();
-        model.addAttribute("salesRep", user);
+        model.addAttribute("user", user);
         model.addAttribute("header", "Add");
         return "salesRepDetail";
     }
-    
+
     @PostMapping("/addSalesRep")
     public String addNewSalesRep(User user) {
         user.setPassword(encoder.encode(user.getPassword()));
         uServ.addUser(user);
         return "redirect:/home";
     }
-    
+
     //Editing SalesRep
     @GetMapping("/salesRep/{username}")
     public String loadEditSalesRep(Model model, @PathVariable String username) {
         User user = uServ.findUserByUsername(username);
-        model.addAttribute("salesRep", user);
+        model.addAttribute("user", user);
         model.addAttribute("header", "Edit");
         return "salesRepDetail";
     }
-    
+
     @PostMapping("/salesRep/{username}")
     public String editSalesRep(@PathVariable String username, User user) {
         uServ.editUser(user);
         return "redirect:/home";
     }
-    
+
     //Adding SalesVisit
     @GetMapping("/addSalesVisit")
     public String viewAddSalesVisit(Model model) {
@@ -147,13 +147,13 @@ public abstract class MainController {
         model.addAttribute("customers", customers);
         return "salesVisitDetail";
     }
-    
+
     @PostMapping("/addSalesVisit")
     public String processAddSalesVisit(SalesVisit visit) {
         svServ.addSalesVisit(visit);
         return "redirect:/home";
     }
-    
+
     //Adding Bill
     @GetMapping("/addBill")
     public String viewAddBill(Model model, Bill bill) {
@@ -163,23 +163,46 @@ public abstract class MainController {
         model.addAttribute("products", products);
         return "billDetail";
     }
-    
+
     @PostMapping("/addBill")
     public String processAddBill(Principal principal, Bill bill) {
         bServ.addBill(bill);
         return "redirect:/home";
     }
-    
-    
+
     private boolean userIsAdmin() {
         List<Role> userRoles = currentUser.getRoles();
         boolean isAdmin = false;
-        for(Role r : userRoles) {
-            if(r.getRole() == "ROLE_ADMIN") {
+        for (Role r : userRoles) {
+            if (r.getRole() == "ROLE_ADMIN") {
                 isAdmin = true;
             }
         }
         return isAdmin;
     }
-    
+
+    // new stuff 2/14/2020 11:30a.m.
+    @GetMapping("/salesRepProfile/{username}")
+    public String loadSalesRepProfile(Model model, @PathVariable String username) {
+        User user = uServ.findUserByUsername(username);
+        model.addAttribute("user", user);
+        return "salesRepProfile";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/salesRep/")
+    public String allSalesReps(Model model) {
+        List<User> allSalesReps = uServ.findAllSalesReps();
+        model.addAttribute("users", allSalesReps);
+        return "salesRep";
+    }
+
+    @GetMapping("/")
+    public String loadBasePage(Model model) {
+        return "redirect:/home";
+    }
 }
